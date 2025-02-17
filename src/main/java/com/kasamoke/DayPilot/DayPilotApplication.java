353dd5bc083c17a9d40baa91229e285500ef8569
Niraj Kumar Pandey <nirajpandey034@gmail.com>
2025-02-17 14:47:09 +0530

    Initial Commit

---
+++ src/main/java/com/kasamoke/DayPilot/DayPilotApplication.java
@@ -0,0 +1,13 @@
+package com.kasamoke.DayPilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayPilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayPilotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kasamoke/DayPilot/DayPilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kasamoke.DayPilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DayPilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
